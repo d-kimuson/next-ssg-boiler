146128585b9c9dc5c4fd106e12348dda3a209487
kimuson <dev.kimuson@gmail.com>
2021-02-03 23:29:25 +0900

    [update] add aspida

---
+++ src/utils/apiClient.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+import aspida from "@aspida/axios"
+import api from "@api/$api"
+
+export const apiClient = api(
+  aspida(axios, {
+    baseURL:
+      process.env.NODE_ENV === "production"
+        ? "https://example.com"
+        : "http://127.0.0.1:8000",
+  })
+)
