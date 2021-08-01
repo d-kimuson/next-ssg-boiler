c23a1cb390a528b20885fcdd2e3a029bc7fb5973
d-kimsuon <d-kimuson@gmail.com>
2021-08-01 15:38:10 +0900

    feat(linter): linter 類をまとめてアップデート

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,4 +5,8 @@ module.exports = {
     "stylelint-config-prettier",
   ],
   plugins: ["stylelint-scss"],
+  rules: {
+    // dart-sass だと use を先に使うので、不要なルール
+    "no-invalid-position-at-import-rule": null,
+  },
 }
